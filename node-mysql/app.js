1c80fdc3c4d5cdf4de81f7d603c37bad9e887f63
wengcy <1459017028@qq.com>
2019-10-19 13:48:31 +0800

    node 服务添加vue入口页面

--- node-mysql/app.js
+++ node-mysql/app.js
@@ -1,8 +1,15 @@
 let express=require("express");
 let db = require('./config/db');
 let app= require('./routes/index');
+const fs = require('fs');
+const path = require('path');
 let cors = require('cors');
 app.use(cors());
-app.listen(8000,"172.16.100.87",function(){
+app.use(express.static(path.resolve(__dirname, './dist')))
+app.get('/', function(req, res) {
+    const html = fs.readFileSync(path.resolve(__dirname, './dist/index.html'), 'utf-8')
+    res.send(html)
+})
+app.listen(80,"172.16.100.87",function(){
     console.log("服务开启成功")
 });
