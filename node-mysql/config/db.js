439c083f35ade84cd444e08894791691463e9027
wengcy <1459017028@qq.com>
2019-10-30 09:16:32 +0800

    再次提交

--- node-mysql/config/db.js
+++ node-mysql/config/db.js
@@ -1,22 +1,25 @@
 let mysql = require('mysql')
-let express = require('express')
+
 const sqlConfig = {
         host: "localhost",
         user: "root", 
         password: "root", 
         database: "mysql" 
-} 
-
-  //连接数据库
-  let connection = mysql.createConnection(sqlConfig);
-	//开始链接数据库
-   connection.connect(function(err){
-		if(err){
-			console.log(`mysql连接失败: ${err}!`);
-		}else{
-			console.log("mysql连接成功!");
-		}
-	});
-	
-	
-	module.exports = connection
+}
+let pool = mysql.createPool(sqlConfig);
+let query = function(sql,callback){
+    pool.getConnection(function(err,conn){
+        if(err){
+			console.log("数据库连接失败")
+            callback(err,null);
+        }else{
+			console.log("数据库连接成功")
+            conn.query(sql,function(err,results){
+                callback(err,results);
+            });
+            conn.release();
+        }
+    });
+};
+ 
+module.exports = query;
--- node-mysql/controller/price.js
+++ node-mysql/controller/price.js
@@ -1,13 +1,11 @@
-var express = require('express')
-let connection = require('../config/db')
-const request = require('request')
+let query = require('../config/db')
 const util = require('../public/util')
 let price = {
 	queryAllPrice: function(req,res) {
 		console.log()
 		let sqlQuery="select id,supplier,name,trend,price,freight,endPrice,density from price where flag = 1 order by supplier,name";
 		let data = {};
-		connection.query(sqlQuery,function(err,result){
+		query(sqlQuery,function(err,result){
 		    if(err){
 			   console.log(`SQL error: ${err}!`);
 			   data.code = "-200";
@@ -32,8 +30,7 @@ let price = {
 		let time = util.getNowFormatDate();
 		
 		let sqlQuery=`insert into price (supplier,name,trend,price,density,createTime,updateTime) values ('${supplier}','${name}','${trend}',${price},${density},'${time}','${time}')`;
-		//let sqlQuery=`insert into price (supplier,name,trend,price,density,createTime,updateTime) values ('中石油','#98','涨',777.000,0.988,'${time}','${time}')`;
-		connection.query(sqlQuery,function(err,result){
+		query(sqlQuery,function(err,result){
 		    if(err){
 		    console.log(err)
 		       data.code = "-200";
@@ -56,8 +53,7 @@ let price = {
 		let density = req.query.density; 
 		let time = util.getNowFormatDate();
 		let sqlQuery=`update price set supplier = '${supplier}', name = '${name}', trend = '${trend}', price = ${price}, density = ${density},updateTime = '${time}' where id = '${id}'`;
-		//let sqlQuery=`update price set supplier = '中国石油', name = '90#', trend = '涨', price = 123456, density = 0.0987 ,updateTime = '${time}' where id = 2`;
-		connection.query(sqlQuery,function(err,result){
+		query(sqlQuery,function(err,result){
 			if(err){
 				 console.log(err)
 				data.code = "-200";
@@ -75,8 +71,7 @@ let price = {
 		let id = req.query.id; 
 		let time = util.getNowFormatDate();
 		let sqlQuery=`update price set flag = 0,updateTime = '${time}' where id = ${id}`
-		//let sqlQuery=`update price set flag = 0,updateTime = '${time}' where id = 2`;
-		connection.query(sqlQuery,function(err,result){
+		query(sqlQuery,function(err,result){
 		    if(err){
 				data.code = "-200";
 				data.message = "数据删除失败"
